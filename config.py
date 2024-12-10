eac623012faf596b62e11b29ca869731bf9e4b92
HUNTER_KARAN <KUMAR663959@GMAIL.COM>
2024-12-10 19:46:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = "9a098f01aa56c836f2e34aee4b7ef963"
 BOT_TOKEN = "7825465869:AAEFYQ_rVGLQTcXHJkxktYCa0I5uQaXVuTY"
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = "mongodb+srv://dnmusic97:HAMENHIPATAPASSWORD%268191@testingdb.bgb0v.mongodb.net/?retryWrites=true&w=majority&appName=Testingdb"
+MONGO_DB_URI = "mongodb+srv://hunterkaran:hY2eMHfPzQ5thH8x@cluster0.14roz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 10000))
 
